e3c3d650d1aca675bf4bc39e54876a6e391956e2
Motoess <synz1234567@gmail.com>
2023-04-06 16:30:23 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -128,7 +128,7 @@ var l_musicVolume = 100;
 
 // Enable custom messages?
 // 'true' or 'false'
-var l_messagesEnabled = true;
+var l_messagesEnabled = false;
 
 // Enter your custom messages below
 var l_messages = [
